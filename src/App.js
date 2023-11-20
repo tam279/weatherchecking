259f22f24bba67bec1417f154276ddbc5ff48b55
Tam Nguyen <87322565+tam279@users.noreply.github.com>
2023-11-20 02:19:55 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const api = {
+  key: "7c2befb2654176fe1659820d457b0a55",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+function App() {
+  const [searchInput, setSearchInput] = useState("");
+  const [searchCity, setSearchCity] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      if (!searchCity) return;
+      setLoading(true);
+      try {
+        const url = `${api.base}weather?q=${searchCity}&units=metric&APPID=${api.key}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        if (response.ok) {
+          setWeatherInfo(
+            `${data.name}, ${data.sys.country} ${data.weather[0].description}, ${data.main.temp}`
+          );
+          setErrorMessage("");
+        } else {
+          setErrorMessage(data.message);
+        }
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
+      setLoading(false);
+    };
+    fetchWeatherData();
+  }, [searchCity]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchCity(searchInput);
+  };
+
+  return (
+    <div className="weather-app">
+      <h1>Weather Checker</h1>
+      <p>To check the weather in your city, please input your city below.</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="City"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button>Search</button>
+      </form>
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <>
+          {errorMessage ? (
+            <div className="error">{errorMessage}</div>
+          ) : (
+            <div className="weather-info">{weatherInfo}</div>
+          )}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
